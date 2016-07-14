f05ab6aee92f69a4ba7bca395070062165210122
myersultan <y.medetov@gmail.com>
2016-07-14 22:27:32 +0600

    object Address added

--- src/day4/hash/Address.java
+++ src/day4/hash/Address.java
@@ -9,31 +9,47 @@ public class Address {
     private String street;
     private int house;
 
-    public Address(){
+    public Address(String city, String street, int house){
+        if (city == null || street == null || house <= 0){
+            throw new IllegalStateException("Parameters should not contain null values and house should be > 0");
+        }
+        this.city = city;
+        this.street = street;
+        this.house = house;
 
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Address){
+            Address addr = (Address) obj;
+            if (city.equals(addr.getCity()) && street.equals(addr.getStreet()) && house == addr.getHouse()){
+               return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 25;
+        result = 37 * result + city.hashCode();
+        result = 37 * result + street.hashCode();
+        result = 37 * result + house;
+        return result;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
     }
 
     public int getHouse() {
         return house;
     }
 
-    public void setHouse(int house) {
-        this.house = house;
-    }
+
 }
